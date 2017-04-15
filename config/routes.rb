25d2a26c6e18047391d9bd703dbe77b193a7766f
yongjia <linyongjia0@live.cn>
2017-04-16 04:12:42 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups do
     resources :posts
+
+    member do
+      post :join
+      post :quit
+    end
+
   end
     root 'groups#index'
 end

ffeef7a1f3614514f3bcb14e89fcad7563b33ab6
yongjia <linyongjia0@live.cn>
2017-04-16 21:29:33 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
-    resources :posts
-
     member do
       post :join
       post :quit
     end
-
+    resources :posts
   end
+  namespace :account do
+  resources :groups
+end
     root 'groups#index'
 end
